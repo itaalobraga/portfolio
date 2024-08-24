7141afae9e38e08258482f286fdea928f5f10fa4
Ítalo Braga <italo@orbesoft.com.br>
2024-08-24 17:10:11 -0300

    feat: prismic cms config and initial integrations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
 import "@/styles/index.css";
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { poppins, sfPro } from "@/fonts";
 import { ThemeProvider } from "next-themes";
 import { twMerge } from "tailwind-merge";
 import { ThemeSwitch } from "./theme-switch";
-
-export const metadata: Metadata = {
-  title: "Ítalo Braga",
-  description: "Ítalo Braga - Desenvolvedor Front-end",
-};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,62 @@
-import { LucideLink } from "lucide-react";
-import Link from "next/link";
+import { createClient } from "@/prismicio";
+import { asImageSrc } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicLink } from "@prismicio/react";
+import { ArrowUpRight } from "lucide-react";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 
-export default function Home() {
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+
+  const { data } = await client.getSingle("home");
+
+  const { name, meta_title, meta_image, meta_description } = data;
+
+  return {
+    title: meta_title,
+    description: meta_description,
+    creator: name,
+    authors: {
+      name: name ?? "Ítalo Braga",
+      url: "https://italobraga.com",
+    },
+    openGraph: {
+      type: "website",
+      url: "https://italobraga.com",
+      title: meta_title ?? "Ítalo Braga",
+      description: meta_description ?? "Ítalo Braga - Desenvolvedor Front-end",
+      siteName: meta_title ?? "Ítalo Braga",
+      images: [
+        {
+          url: asImageSrc(meta_image) ?? "",
+        },
+      ],
+    },
+  };
+}
+
+export default async function Home() {
+  const client = createClient();
+
+  const { data } = await client.getSingle("home").catch(notFound);
+
+  const { avatar, name, role, about, links } = data;
+
   return (
     <main className="bg flex min-h-screen flex-col justify-center">
       <div className="m-auto flex w-full max-w-[31.25rem] flex-col gap-[2.5rem]">
         <section>
           <div className="flex items-center gap-[1.5rem]">
-            <div className="h-[6.25rem] w-[6.25rem] rounded-full bg-red-300 transition-transform hover:scale-95" />
+            <PrismicNextImage
+              field={avatar}
+              className="h-[6.25rem] w-[6.25rem] rounded-full object-contain transition-transform hover:scale-95"
+            />
 
             <div>
-              <h1 className="h1 text-[1.5rem]">Ítalo Braga</h1>
+              <h1 className="h1 text-[1.5rem]">{name}</h1>
 
-              <h2 className="h2 text-[1rem]">Desenvolvedor Front-end</h2>
+              <h2 className="h2 text-[1rem]">{role}</h2>
             </div>
           </div>
         </section>
@@ -20,31 +64,29 @@ export default function Home() {
         <section>
           <h3 className="h3 mb-[1rem] text-[1rem]">Sobre</h3>
 
-          <p className="p text-[0.875rem]">
-            Com mais de dois anos de experiência como desenvolvedor front-end,
-            eu tenho participado de projetos desafiadores e inovadores,
-            utilizando tecnologias como React e React Native. Meu objetivo é
-            criar aplicações que ofereçam uma ótima experiência para os usuários
-            e que atendam às necessidades dos clientes.
-          </p>
+          <p className="p text-[0.875rem]">{about}</p>
         </section>
 
         <section>
           <h3 className="h3 mb-[1rem] text-[1rem]">Links</h3>
 
-          <div className="flex items-center gap-[2.5rem]">
-            <span className="span text-[0.875rem]">Github</span>
+          {links.map(({ label, link, title }) => {
+            return (
+              <div key={label} className="flex items-center gap-[2.5rem]">
+                <span className="span text-[0.875rem]">{title}</span>
 
-            <Link
-              href="https://github.com/itaalobraga"
-              rel="noopener noreferrer"
-              className="link flex items-center gap-[0.5rem] text-[0.875rem] hover:underline"
-              target="_blank"
-            >
-              @itaalobraga
-              <LucideLink className="link size-[0.875rem]" />
-            </Link>
-          </div>
+                <PrismicLink
+                  field={link}
+                  rel="noopener noreferrer"
+                  className="link flex items-center gap-[0.5rem] text-[0.875rem] hover:underline"
+                  target="_blank"
+                >
+                  {label}
+                  <ArrowUpRight className="link size-[0.875rem]" />
+                </PrismicLink>
+              </div>
+            );
+          })}
         </section>
       </div>
     </main>
